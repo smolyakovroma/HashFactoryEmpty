f456c5266bca82078e675e48dea26f2e0253f50a
Roman <smolyakovroma@gmail.com>
2017-06-26 18:51:38 +0700

    - добавил ajax send contact - убрал всплывающую форму "в разработке"

--- src/main/java/ru/hashfactory/empty/MainRestController.java
+++ src/main/java/ru/hashfactory/empty/MainRestController.java
@@ -3,6 +3,7 @@ package ru.hashfactory.empty;
 import org.apache.commons.mail.EmailException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +27,26 @@ public class MainRestController {
             }
         }).start();
 
-
-//      return new ResponseEntity<String>("error", HttpStatus.INTERNAL_SERVER_ERROR);
-
-//        return new ResponseEntity<String>("Ok", HttpStatus.OK);
     }
+
+//    @RequestMapping(value = "/sendMail/{name}/{mail}")
+//    public void sendMail(@PathVariable("name") String name, @PathVariable("mail") String mail){
+////        ResponseEntity<String>
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    MailConfig.send(name, mail);
+//                } catch (EmailException e) {
+//                    e.printStackTrace();
+//
+//                }
+//            }
+//        }).start();
+//
+//
+////      return new ResponseEntity<String>("error", HttpStatus.INTERNAL_SERVER_ERROR);
+//
+////        return new ResponseEntity<String>("Ok", HttpStatus.OK);
+//    }
 }
